83834f47c7b83f65fa5f3dc74b10fd06f03d03dc
みやしたちひろ <c09069354869@gmail.com>
2019-06-13 08:25:32 +0800

    更新機能追加
    更新のため

--- update.php
+++ update.php
@@ -5,9 +5,9 @@ require_once('Models/Todo.php');
 $task = $_POST['task'];
 $id = $_POST['id'];
 
-$todo = new Todo();
+$todo = new Todo;
 
-$todo->update($task,$id);
+// $todo->update($task,$id);
 
 header('Location:index.php');
 
